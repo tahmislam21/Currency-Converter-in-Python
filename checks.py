f1fad2afbb7a4ee0def6c305a1a83f577916b089
tahmislam21 <107170166+tahmislam21@users.noreply.github.com>
2025-01-14 11:34:56 +0600

    Update checks.py

--- checks.py
+++ checks.py
@@ -6,21 +6,6 @@ def check_arguments(args):
     Function that will check if there are enough input arguments provided (ie exactly 3) and will return the input arguments if it is the case.
     Otherwise the program will exit and display the relevant message provided in the assignment brief
 
-    Parameters
-    ----------
-    # => To be filled by student
-    list
-
-    Pseudo-code
-    ----------
-    # => To be filled by student
-    check if length of list is equal to 3 components
-    if not, print error message and exit the program
-
-    Returns
-    -------
-    # => To be filled by student
-    NULL
     """
 
     # => To be filled by student
@@ -34,22 +19,7 @@ def check_date(date):
     Function that will check if the provided date is valid and will return the value True if that is the case. 
     Otherwise the program will exit and display the relevant message provided in the assignment brief
 
-    Parameters
-    ----------
-    # => To be filled by student
-    str(date)
-
-    Pseudo-code
-    ----------
-    # => To be filled by student
-    check if date is in the correct format
-    if not, print error message and exit the program
-
-    Returns
-    -------
-    # => To be filled by student
     """
-    # => To be filled by student
     try:
         datetime.strptime(date, '%Y-%m-%d')
     except:
